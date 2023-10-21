9bb39505890c4fe2a45ff05fea8c18df142ebdb5
Jakub Ćwikowski <kcwikowski007@yahoo.com>
2023-10-21 12:16:36 +0200

    chore: lint code

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/category-form.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/category-form.tsx
@@ -17,7 +17,6 @@ import {
 import toast from 'react-hot-toast'
 import { useRouter } from 'next/navigation'
 import { cn } from '@/lib/utils'
-import { Textarea } from '@/components/ui/textarea'
 import { Course } from '@prisma/client'
 import { Combobox } from '@/components/ui/combobox'
 
