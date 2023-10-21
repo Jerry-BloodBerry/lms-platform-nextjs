bbc70e152bf21a0cfadd7f1786ae5ef372433408
Jakub Ćwikowski <kcwikowski007@yahoo.com>
2023-10-21 11:47:24 +0200

    feat: add category form to course creation page

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -6,6 +6,7 @@ import { redirect } from 'next/navigation'
 import { TitleForm } from './_components/title-form'
 import { DescriptionForm } from './_components/description-form'
 import { ImageForm } from './_components/image-form'
+import { CategoryForm } from './_components/category-form'
 
 const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
   const { userId } = auth()
@@ -17,6 +18,12 @@ const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
   const course = await db.course.findUnique({
     where: {
       id: params.courseId,
+    },
+  })
+
+  const categories = await db.category.findMany({
+    orderBy: {
+      name: 'asc',
     },
   })
 
@@ -57,6 +64,14 @@ const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
         <TitleForm initialData={course} courseId={params.courseId} />
         <DescriptionForm initialData={course} courseId={params.courseId} />
         <ImageForm initialData={course} courseId={params.courseId} />
+        <CategoryForm
+          initialData={course}
+          courseId={params.courseId}
+          options={categories.map(category => ({
+            label: category.name,
+            value: category.id,
+          }))}
+        />
       </div>
     </div>
   )
