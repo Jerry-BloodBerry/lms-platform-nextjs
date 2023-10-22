0d8868ad50554885cf2a0f1813396db542e16c73
Jakub Ćwikowski <kcwikowski007@yahoo.com>
2023-10-22 12:20:37 +0200

    feat: finish chapters form

--- app/api/courses/[courseId]/chapters/route.ts
+++ app/api/courses/[courseId]/chapters/route.ts
@@ -1,10 +1,10 @@
-import { db } from "@/lib/db";
-import { auth } from "@clerk/nextjs";
-import { NextResponse } from "next/server";
+import { db } from '@/lib/db'
+import { auth } from '@clerk/nextjs'
+import { NextResponse } from 'next/server'
 
 export async function POST(
   req: Request,
-  { params }: { params: { courseId: string } }
+  { params }: { params: { courseId: string } },
 ) {
   try {
     const { userId } = auth()
@@ -40,8 +40,8 @@ export async function POST(
       data: {
         title,
         courseId: params.courseId,
-        position: newPosition
-      }
+        position: newPosition,
+      },
     })
 
     return NextResponse.json(chapter)
